74f93ed0107e915743f992d63e17590a1d87ef9a
Johan Schierbeck <johan@schilie.se>
2014-02-11 01:35:21 +0100

    Added comments

--- call_me.php
+++ call_me.php
@@ -1,13 +1,17 @@
 <?php
-
+// Vi hämtar upp datan vi skickade från javascripten men $_POST['review']
+// Nycklarna i objekten blir också under arrayer till $_POST['review'], exempelvis $_POST['review']['name']
 $name = $_POST['review']['name'];
 $food = $_POST['review']['food'];
 $like = $_POST['review']['like'];
 
-echo ' > ' . $name . ' ';
+// Det vi nedan skriver (echoar) ut, är vad som kommer tillbaka till javascript filen som variabeln data
 
+echo ' > ' . $name . ' ';
+// Om $_POST['review']['like'] var 1
 if( $like == 1 ) {
 	echo 'gillar ';
+// Om $_POST['review']['like'] var 0
 } else {
 	echo 'ogillar ';
 }
--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
 $(document).ready(function(){
 
 	function get_values() {
+		// Skapar ett objekt med tre nycklar som refererar till värden från två input fält och en select
 		var my_object = {
 			name: $('#name').val(),
 			food: $('#food').val(),
 			like: $('#like').val()
 		}
+		// returnerar objektet
 		return my_object;
 	}
 
+	// Vis klick på länk
 	$('a').click(function(e) {
+		// Denna rad avbryter att länken ska fungera som vanligt, dvs gå till en annan sida eller ladda om samma sida
 		e.preventDefault();
 
+		// Vi skickar en post variabel som php kan snappa upp med $_POST['review']
+		// Med variabeln data får vi ut informationen som skickas tillbaka från call_me.php
 		$.post('call_me.php', { review: get_values(),  }, function(data) {
-			console.log(data);
+			// Skriv ut i divven
 			$('div').prepend(data + '<br>');
 
 		});
